1f532b0bdbd83e0359b649398e7840f9d0d8c760
Axtemka <115342104+Axtemka@users.noreply.github.com>
2022-11-11 19:39:34 +0300

    Initial commit

--- app/src/main/java/com/example/kotlinprojecttest2/data/remote/quest/QuestApi.kt
+++ app/src/main/java/com/example/kotlinprojecttest2/data/remote/quest/QuestApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.Headers
 
 
 interface QuestApi {
-    @GET("./wall.get?domain=sciencemem&count=0&access_token=9fb466189fb466189fb46618449ca5442599fb49fb46618fce51db6b049cb80918bb78e&v=5.131")
+    @GET("./wall.get?domain=memzavod1523l&count=0&access_token=9fb466189fb466189fb46618449ca5442599fb49fb46618fce51db6b049cb80918bb78e&v=5.131")
     fun get_vk_json() : Single<GetJsonResponse>
 }
